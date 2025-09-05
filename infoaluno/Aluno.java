29b53138f1fb80f9730a273a14fabafdf97ecdb5
João Victor Fernandes De Oliveira <jv042002@gmail.com>
2025-09-05 12:00:41 -0300

    "Adicionando classes iniciais".

---
+++ infoaluno/Aluno.java
@@ -0,0 +1,14 @@
+package infoaluno;
+
+public class Aluno {
+    private String nome;
+
+    public void setNome(String novoNome) {  // Definir o nome
+        nome = novoNome;
+    }
+
+    // Obter o nome
+    public String getNome() {
+        return nome;
+    }
+}
---
+++ infoaluno/Secretaria.java
@@ -0,0 +1,15 @@
+
+package infoaluno;
+
+
+class Secretaria {
+    public static void main(String[] args) {
+
+        Aluno aluno = new Aluno();
+
+        aluno.setNome("Maria");
+
+        System.out.println("Nome do aluno: " + aluno.getNome());
+
+    }
+}
